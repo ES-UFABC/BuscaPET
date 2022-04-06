88653d802512ec7577b0b85023c565c0d75b2b46
Lucca Jacomassi <lucca.jacomassi@hotmail.com>
2022-04-05 23:43:03 -0300

    update project to androidx and add user account screen

--- app/src/main/java/com/example/buscapet/UserAccount.java
+++ app/src/main/java/com/example/buscapet/UserAccount.java
@@ -1,15 +1,15 @@
 package com.example.buscapet;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+public class UserAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cadastro);
+        setContentView(R.layout.activity_user_account);
 
         getSupportActionBar().hide();
     }
