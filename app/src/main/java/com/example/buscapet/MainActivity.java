7d4d7dcf0d0ae3b71f873597b25034662042b383
thais.larissa.b.andrade@gmail.com <thais.larissa.b.andrade@gmail.com>
2022-04-27 08:21:51 -0300

    add funcao de deslogar

--- app/src/main/java/com/example/buscapet/MainActivity.java
+++ app/src/main/java/com/example/buscapet/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.buscapet;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nomeUsuario,emailUsuario;
+    private Button bt_deslogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        IniciarComponentes();
+
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent( MainActivity.this,SignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        nomeUsuario = findViewById(R.id.userName);
+        emailUsuario = findViewById(R.id.userEmail);
+        bt_deslogar = findViewById(R.id.bt_deslogar);
     }
 }
