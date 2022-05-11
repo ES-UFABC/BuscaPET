4ad94b1405d6a49fb661e41819de6efb22e15822
thais.larissa.b.andrade@gmail.com <thais.larissa.b.andrade@gmail.com>
2022-05-11 00:07:22 -0300

    add recuperacao de nome e email

--- app/src/main/java/com/example/buscapet/UserAccount.java
+++ app/src/main/java/com/example/buscapet/UserAccount.java
@@ -1,5 +1,6 @@
 package com.example.buscapet;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -8,10 +9,17 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class UserAccount extends AppCompatActivity {
     private TextView nomeUsuario,emailUsuario;
     private Button bt_deslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,24 @@ public class UserAccount extends AppCompatActivity {
                 Intent intent = new Intent( UserAccount.this,SignIn.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection( collectionPath: "Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if(documentSnapshot != null) {
+                    nomeUsuario.setText(documentSnapshot.getString( field: "nome"));
+                    emailUsuario.setText(email);
+                }
             }
         });
     }
