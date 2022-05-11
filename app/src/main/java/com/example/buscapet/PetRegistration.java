5809949c0c21acad73eaab6fc477c98a37fc32e1
Lucca Jacomassi <56274210+lulcca@users.noreply.github.com>
2022-05-10 22:30:46 -0300

    add upload picture option (#22)

--- app/src/main/java/com/example/buscapet/PetRegistration.java
+++ app/src/main/java/com/example/buscapet/PetRegistration.java
@@ -1,10 +1,9 @@
 package com.example.buscapet;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -13,6 +12,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,26 +25,37 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.jaredrummler.materialspinner.MaterialSpinner;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class PetRegistration extends AppCompatActivity {
-
-    private EditText labelCity,labelAddress,labelTags;
-    private MaterialSpinner labelAnimal,labelState;
     private Button signInButton;
     private CheckBox checkHome;
+    private EditText labelCity,labelAddress,labelTags;
+    private FirebaseStorage storage;
+    private ImageView profilePic;
+    private MaterialSpinner labelAnimal,labelState;
+    private StorageReference storageReference;
     private String IDpet;
-
+    public Uri imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_registration);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        profilePic = findViewById(R.id.labelPicture);
+        profilePic.setOnClickListener(view -> choosePicture());
 
         getSupportActionBar().hide();
         IniciarComponentes();
@@ -172,5 +188,45 @@ public class PetRegistration extends AppCompatActivity {
         Intent intent = new Intent(PetRegistration.this,UserAccount.class);
         startActivity(intent);
         finish();
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageURI = data.getData();
+            profilePic.setImageURI(imageURI);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+
+        final ProgressDialog prss = new ProgressDialog(this);
+        prss.setTitle("Enviando...");
+        prss.show();
+
+        final String key = UUID.randomUUID().toString();
+        StorageReference imageRef = storageReference.child("images/" + key);
+        imageRef.putFile(imageURI)
+                .addOnSuccessListener(snapshot -> {
+                    prss.dismiss();
+                    Snackbar.make(findViewById(android.R.id.content), "Imagem enviada!", Snackbar.LENGTH_LONG).show();
+                })
+                .addOnFailureListener(e -> {
+                    prss.dismiss();
+                    Toast.makeText(getApplicationContext(), "Falha ao enviar.", Toast.LENGTH_LONG).show();
+                })
+                .addOnProgressListener(snapshot -> {
+                    double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    prss.setMessage("Carregando: " + (int) progressPercent + "%");
+                });
     }
 }
